a7428f7f8fd2da941703ebad29f7ebefc8153aa0
Connor Younglund <cdy@airmail.cc>
2021-06-26 13:28:37 -0400

    fix images not rendering

--- src/components/ClueTable.tsx
+++ src/components/ClueTable.tsx
@@ -4,6 +4,10 @@ const ClueTable: React.FC<{ clues: Clue[] | undefined; type: string }> = ({
   clues,
   type,
 }) => {
+  const ClueHint = ({ hint }: { hint: string }) => {
+    return hint.startsWith('http') ? <img src={hint} /> : <>{hint}</>;
+  };
+
   return clues && clues.length ? (
     <>
       <h2>{type} Clues</h2>
@@ -24,7 +28,9 @@ const ClueTable: React.FC<{ clues: Clue[] | undefined; type: string }> = ({
             ({ clueHint, itemsRequired, location, solution, type }) => (
               <tr key={clueHint}>
                 <td>{type}</td>
-                <td>{clueHint}</td>
+                <td>
+                  <ClueHint hint={clueHint} />
+                </td>
                 <td>{solution}</td>
                 <td>{location}</td>
                 <td>{itemsRequired?.join(', ')}</td>
