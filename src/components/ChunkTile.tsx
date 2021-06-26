1d6fbacdf1b7b2ccbea5d4e409b4f207965933f2
Connor Younglund <cdy@airmail.cc>
2021-06-26 15:52:08 -0400

    fix opening chunk modal on mobile devices

--- src/components/ChunkTile.tsx
+++ src/components/ChunkTile.tsx
@@ -23,19 +23,37 @@ const ChunkTile: React.FC<{
     moveDistanceRef.current = d;
   };
 
+  // mobile initial touch point
+  const initialTouchRef = useRef({ x: 0, y: 0 });
+  const setInitialTouch = (t: { x: number; y: number }) => {
+    initialTouchRef.current = t;
+  };
+
   function resetMouseState() {
     setMoveDistance(0);
+    setInitialTouch({ x: 0, y: 0 });
     setMouseDown(false);
   }
 
-  function mouseDownHandler(e: MouseEvent) {
+  function mouseDownHandler(e: MouseEvent | TouchEvent) {
+    const mobilePress = e.type === 'touchstart';
+
     // if the left mouse button was pressed
-    if (e.button === 0) setMouseDown(true);
+    if (mobilePress || (e as MouseEvent).button === 0) {
+      if (mobilePress) {
+        const touch = (e as TouchEvent).touches[0];
+        setInitialTouch({ x: touch.pageX, y: touch.pageY });
+      }
+      setMouseDown(true);
+    }
   }
 
-  function mouseUpHandler(e: MouseEvent) {
+  function mouseUpHandler(e: MouseEvent | TouchEvent) {
     // if the left mouse button was released
-    if (mouseDownRef.current && e.button === 0) {
+    if (
+      mouseDownRef.current &&
+      (e.type === 'touchend' || (e as MouseEvent).button === 0)
+    ) {
       // only trigger `onClick` when the user isn't moving the map
       if (onClick && moveDistanceRef.current <= 10) {
         onClick();
@@ -45,20 +63,36 @@ const ChunkTile: React.FC<{
     }
   }
 
-  function mouseMoveHandler(e: MouseEvent) {
+  function mouseMoveHandler(e: MouseEvent | TouchEvent) {
     // do nothing if the left mouse button isn't held down
     if (!mouseDownRef.current) return;
 
-    const a = e.movementX;
-    const b = e.movementY;
+    function distance(a: number, b: number) {
+      return Math.sqrt(a * a + b * b);
+    }
 
-    setMoveDistance(moveDistanceRef.current + Math.sqrt(a * a + b * b));
+    if (e.type === 'mousemove') {
+      const mouseEvent = e as MouseEvent;
+
+      const a = mouseEvent.movementX;
+      const b = mouseEvent.movementY;
+
+      setMoveDistance(moveDistanceRef.current + distance(a, b));
+    } else if (e.type === 'touchmove') {
+      const touch = (e as TouchEvent).touches[0];
+
+      const a = touch.pageX - initialTouchRef.current.x;
+      const b = touch.pageY - initialTouchRef.current.y;
+
+      setMoveDistance(distance(a, b));
+    }
   }
 
   useEffect(() => {
     const tdEl = tdRef.current;
     if (!tdEl) return;
 
+    // desktop
     tdEl.addEventListener('mousedown', mouseDownHandler);
     tdEl.addEventListener('mouseup', mouseUpHandler);
 
@@ -66,6 +100,14 @@ const ChunkTile: React.FC<{
 
     tdEl.addEventListener('mouseenter', resetMouseState);
     tdEl.addEventListener('mouseleave', resetMouseState);
+
+    // mobile
+    tdEl.addEventListener('touchstart', mouseDownHandler);
+    tdEl.addEventListener('touchend', mouseUpHandler);
+
+    tdEl.addEventListener('touchmove', mouseMoveHandler);
+
+    tdEl.addEventListener('touchcancel', resetMouseState);
   }, [tdRef]);
 
   return (
