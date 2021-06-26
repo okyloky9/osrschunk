aaae36db99bd8c325768d103287d13b7ca59809c
Connor Younglund <cdy@airmail.cc>
2021-06-26 15:26:31 -0400

    display alternate chunks info

--- src/components/ClueTable.tsx
+++ src/components/ClueTable.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactTooltip from 'react-tooltip';
+
 import { Clue, ClueDifficulty } from '../models';
 import ClueIcon from './ClueIcon';
 
@@ -24,12 +27,20 @@ const ClueTable: React.FC<{
             <th>Solution</th>
             <th>Location</th>
             <th>Items</th>
+            <th>Alternate Chunks</th>
           </tr>
         </thead>
 
         <tbody>
           {clues.map(
-            ({ clueHint, itemsRequired, location, solution, type }) => (
+            ({
+              alternateChunks,
+              clueHint,
+              itemsRequired,
+              location,
+              solution,
+              type,
+            }) => (
               <tr key={clueHint}>
                 <td>{type}</td>
                 <td>
@@ -38,6 +49,24 @@ const ClueTable: React.FC<{
                 <td>{solution}</td>
                 <td>{location}</td>
                 <td>{itemsRequired?.join(', ')}</td>
+                <td>
+                  {alternateChunks?.map((alt, index) => (
+                    <React.Fragment key={`alt-chunk-${alt.x}-${alt.y}`}>
+                      <span
+                        className="alternate-chunk"
+                        data-tip={alt.notes}
+                        data-place="top"
+                        data-type="info"
+                      >
+                        ({alt.x}, {alt.y})
+                      </span>
+                      <ReactTooltip />
+
+                      {alternateChunks.length > 1 &&
+                        index < alternateChunks.length - 1 && <>, </>}
+                    </React.Fragment>
+                  ))}
+                </td>
               </tr>
             )
           )}
