dd411848043c7be2198ae5b5b47b5fc1278d5ada
Connor Younglund <cdy@airmail.cc>
2021-07-13 19:16:41 -0400

    add support for alternate chunks for stash units

--- src/components/StashUnitTable.tsx
+++ src/components/StashUnitTable.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { ItemIcon } from '.';
 import type { StashUnit } from '../models';
 
@@ -29,39 +31,47 @@ const StashUnitTable: React.FC<{ units?: StashUnit[] }> = ({ units }) => {
             <th>Appearance</th>
             <th>Location</th>
             <th>Items</th>
-            <th>Chunk</th>
+            <th>Chunk(s)</th>
           </tr>
         </thead>
 
         <tbody>
-          {units.map(({ difficulty, type, location, items, chunk }) => (
-            <tr key={location}>
-              <td>{difficulty}</td>
-              <td>
-                <div className="stash-unit-type">
-                  <img
-                    className="stash-unit-icon"
-                    src={stashIcons[type]}
-                    aria-hidden
-                  />
-                  <div>{type}</div>
-                </div>
-              </td>
-              <td
-                dangerouslySetInnerHTML={{
-                  __html: location.replaceAll('\n', '<br />'),
-                }}
-              />
-              <td>
-                {items.map((item, index) => (
-                  <ItemIcon item={item} key={index} />
-                ))}
-              </td>
-              <td className="chunk-coords">
-                ({chunk?.x}, {chunk?.y})
-              </td>
-            </tr>
-          ))}
+          {units.map(
+            ({ difficulty, type, location, items, chunk, alternateChunks }) => (
+              <tr key={location}>
+                <td>{difficulty}</td>
+                <td>
+                  <div className="stash-unit-type">
+                    <img
+                      className="stash-unit-icon"
+                      src={stashIcons[type]}
+                      aria-hidden
+                    />
+                    <div>{type}</div>
+                  </div>
+                </td>
+                <td
+                  dangerouslySetInnerHTML={{
+                    __html: location.replaceAll('\n', '<br />'),
+                  }}
+                />
+                <td>
+                  {items.map((item, index) => (
+                    <ItemIcon item={item} key={index} />
+                  ))}
+                </td>
+                <td className="chunk-coords">
+                  ({chunk?.x}, {chunk?.y})
+                  {alternateChunks &&
+                    alternateChunks.map(({ x, y }) => (
+                      <React.Fragment key={`alt-${x}-${y}`}>
+                        <span className="comma">,</span> ({x}, {y})
+                      </React.Fragment>
+                    ))}
+                </td>
+              </tr>
+            )
+          )}
         </tbody>
       </table>
     </div>
--- src/models/stash-unit.ts
+++ src/models/stash-unit.ts
@@ -4,6 +4,7 @@ type StashUnit = {
   location: string;
   items: string[];
   chunk?: { x: number; y: number };
+  alternateChunks?: { x: number; y: number }[];
 };
 
 export default StashUnit;
